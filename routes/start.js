74abee88fad762c978a10f0aef5793c704ca06b7
Wth10 <weuberth10@gmail.com>
2022-06-19 00:08:25 -0300

    UpdateTest

--- routes/start.js
+++ routes/start.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (_req, res, _next) {
-	res.render('views/dash');
+	res.render('views/index');
 });
 
 router.get('*', function (_req, res, _next) {
